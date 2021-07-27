669a56c8a3dbb999ed0f84925423659f31c5806a
Franklin Nkokam Ngongang <ntuifrankln2005@gmail.com>
2021-07-27 00:25:10 -0400

    Fixed the databse connection issue

--- compile_for_production.sh
+++ compile_for_production.sh
@@ -3,10 +3,12 @@
 
 echo "precompiling everything for production environment"
 RAILS_ENV=production rails assets:precompile
+RAILS_ENV=production rake db:migrate 
 
 echo "changing file permission for tmp and public folder"
 chmod -R 777 tmp
 chmod -R 777 public
+chmod -R 755 *
 echo "chaninging ownership and assigning to user nginx for root folder"
 chown -R nginx:nginx  .
 echo "\n"
